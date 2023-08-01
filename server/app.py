089f6c693e6263624be3ec06a51dfef58d9a8d95
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-01 19:36:25 -0400

    Created 4 Tables in Models.py

--- server/app.py
+++ server/app.py
@@ -1,10 +1,1 @@
-from flask import *
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'hello world'
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ server/models.py
@@ -0,0 +1,92 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    # Fields
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Relationships
+    posts = db.relationship("Post", back_populates="user")
+    comments = db.relationship("Comment", back_populates="user")
+    replies = db.relationship("Reply", back_populates="user")
+
+    # Serializer
+    serialize_rules = ('-posts', '-comments', 'replies')
+
+class Post(db.Model, SerializerMixin):
+    __tablename__ = "posts"
+
+    # Fields
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    vote_count = db.Column(db.Integer, default=1)
+    comment_count = db.Column(db.Integer, default=0)
+
+    # Relationships 
+
+    users = db.relationship("User", back_populates="post", cascade="all, delete-orphan")
+    comments = association_proxy("users", "comment", creator=lambda c: User(comment=c))
+
+    # Serializer
+    serialize_rules = ("-users",)
+
+class Comment(db.Model, SerializerMixin):
+
+    # Fields
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    reply_id = db.Column(db.Integer, db.ForeignKey("replies.id"))
+
+    content = db.Column(db.String, nullable=False)
+    vote_count = db.Column(db.Integer, default=1)
+    
+    # Relationships 
+    users = db.relationship("User", back_populates="comment", cascade="all, delete-orphan")
+    reply = db.relationship("Reply", back_populates="comments")
+
+    posts = association_proxy("users", "post", creator=lambda p: User(post=p))
+
+class Reply(db.Model, SerializerMixin):
+    
+    # Fields
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_id =db.Column(db.Integer, db.ForeignKey("comments.id"), nullable=False)
+
+    content = db.Column(db.String, nullable=False)
+    vote_count = db.Column(db.Integer, default=1)
+
+    # Relationships
+    users = db.relationship("User", back_populates="replies", cascade="all, delete-orphan") 
+    comments = db.relationship("Comment", back_populates="replies", cascade="all, delete-orphan")
+    
+    
+
+
+
+
+    
+
