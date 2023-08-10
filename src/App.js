c7f254c0520aebd4df41a0745a51e68ea9607137
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-09 20:15:29 -0400

    Code for Logout, deletes setCurrentUser and redirects user to the homepage

--- src/App.js
+++ src/App.js
@@ -57,6 +57,12 @@ function App() {
     })
   }
   
+  function logout() {
+    setCurrentUser(null)
+    fetch('/logout', { method:"DELETE"})
+    history.push('/')
+  }
+
     return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
     <p className="text-3xl text-gray-700 font-bold mb-5">
