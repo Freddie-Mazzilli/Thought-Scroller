b3a9fe6726ab344d3a9b4da3da7c434db86930c1
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-15 18:49:02 -0400

    CreatePost component with base code

---
+++ src/CreatePost.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreatePost() {
+
+    return (
+        <div className="bg-gray-300">
+            This is where a User creates a post
+        </div>       
+    )
+}
+
+export default CreatePost;
