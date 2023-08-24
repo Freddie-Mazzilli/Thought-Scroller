6d35b287fee8a166c20bdca45f216dd6c2817def
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-23 23:45:15 -0400

    Updated CSS stylings on Signup.js, imported the Mister Brain image and fixed up the form

--- src/Login.js
+++ src/Login.js
@@ -17,7 +17,7 @@ function Login({attemptLogin}) {
     return (
         <div className="grid grid-cols-2 items-center">
             <div>
-                <img className="ml-40" src={MisterBrain} alt="Brainy One"/>
+                <img className="ml-40 md:border-2 border-white" src={MisterBrain} alt="Brainy One"/>
             </div>
             <div className="w-3/4">
                 <h1 className="text-white text-center text-3xl font-extralight">Login Here</h1>
@@ -31,7 +31,7 @@ function Login({attemptLogin}) {
                         <input className="text-black" type="password" onChange={handleChangePassword} value={password} placeholder="Password"/> 
                         <br></br>
                         <br></br>
-                        <button className="text-white md:border-4 border-blue-700 mx-60" type="submit"> Login </button>
+                        <button className="text-white md:border-4 border-white mx-60 py-2" type="submit"> Login </button>
                     </div>
                 </form>
             </div>
--- src/Signup.js
+++ src/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import MisterBrain from "./assets/MisterBrain.png"
 
 function Signup({attemptSignup}) {
 
@@ -15,20 +16,26 @@ function Signup({attemptSignup}) {
 
 
     return (
-        <div>
+        <div className="grid grid-cols-2 items-center">
             <div>
-                <h2 className="text-white">ThoughtScroller</h2>
+                <img className="ml-40 md:border-2 border-white" src={MisterBrain}/>
             </div>
-                <h6 className="text-white">Signup Here</h6>
+            <div className="w-3/4">
+                <h1 className="text-white text-center text-3xl font-extralight">Signup Here</h1>
+                <br></br>
                 <form onSubmit={handleSubmit}>
-                    <div className="bg-blue-500">
+                    <div className="grid grid-rows-5">
                         <label className="text-white">Username</label>
                         <input className="text-black" type="text" onChange={handleChangeUsername} value={username} placeholder="Username"/>
+                        <br></br>
                         <label className="text-white">Password</label>
                         <input className="text-black" type="password" onChange={handleChangePassword} value={password} placeholder="Password"/>
-                        <button type="submit">Signup</button>
+                        <br></br>
+                        <br></br>
+                        <button className="text-white md:border-4 border-white mx-60 py-2" type="submit">Signup</button>
                     </div>
                 </form>
+            </div>
         </div>
     );
 }
